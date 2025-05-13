5edf86e882cc9c9b35a324d562acf8a540fc906a
Mikey Ferguson <mikeytallyferguson@gmail.com>
2025-05-12 19:56:51 -0500

    cleanup

--- solution.rs
+++ solution.rs
@@ -1,6 +1,16 @@
 use std::collections::HashMap;
 
 impl Solution {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////
+
+    /*
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    You can return the answer in any order.
+
+    Link: https://leetcode.com/problems/two-sum/description/
+     */
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         // Create a map to store the indices and values of the numbers
         let mut map: HashMap<i32, i32> = HashMap::new();
@@ -19,4 +29,9 @@ impl Solution {
 
         return vec![]
     }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    
+    
+
 }
